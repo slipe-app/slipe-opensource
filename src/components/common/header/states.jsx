2f5335245d96308f637f3dd4831ea3356e7367cf
Dikiy Perec <hopabsq@mail.ru>
2024-09-25 09:05:00 +0600

    Preparing for auth page and fixed saving switcher position after route change

--- src/components/common/header/states.jsx
+++ src/components/common/header/states.jsx
@@ -7,14 +7,14 @@ export default function StatesRender(url) {
 	const { activeTab, setActiveTab } = useContext(PostsTypeContext);
 
 	useEffect(() => {
-		animate(".active-bg", { left: activeTab === "forYou" ? ".25rem" : "50%" }, { easing: "ease", duration: 0.2 });
-	}, [activeTab]);
+		animate(".active-bg", { left: activeTab === "forYou" ? "0.25rem" : "50%" }, { easing: "ease", duration: 0.2 });
+	});
 	switch (url) {
 		case "/":
 			return (
 				<div style={{ background: colors.buttonInactiveBackground }} className='relative p-1 flex w-64 h-[3.25rem] rounded-full'>
 					{/* Анимируем движущийся фон для активной кнопки */}
-					<div style={{ background: colors.buttonInactiveBackground }} className='absolute w-[calc(50%-0.25rem)] h-[calc(100%-0.5rem)] rounded-full active-bg'></div>
+					<div style={{ background: colors.buttonInactiveBackground, left: "0.25rem" }} className='absolute w-[calc(50%-0.25rem)] h-[calc(100%-0.5rem)] rounded-full active-bg'></div>
 					{/* Кнопки */}
 					<button style={{ color: activeTab === "forYou" ? colors.text : colors.textPrimaryTransparent }} onClick={() => setActiveTab("forYou")} className='flex-1 z-10 font-medium text-center transition-colors duration-300'>
 						For you
