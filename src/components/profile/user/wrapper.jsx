5655f937eff12ea25a5f7a14537b124ce35aec7a
Dikiy Perec <hopabsq@mail.ru>
2024-11-05 14:07:42 +0600

    improved page's fading in animation and blur

--- src/components/profile/user/wrapper.jsx
+++ src/components/profile/user/wrapper.jsx
@@ -6,11 +6,11 @@ import "./wrapper.scss";
 export default function ProfileUserWrapper({ user }) {
 	// Передавай в banner только banner там уже есть все ссылки пупс
 	return (
-		<>
+		<div className="profile-wrapper">
 			<ProfileBanner banner={user} username='dikiy' pixels={[0, 0, 2, 2, 2, 2, 2, 2, 0, 1, 2, 3, 3, 3, 0, 2]} />
-			<div className='profile-wrapper'>
+			<div className='profile-content-wrapper'>
 				<ProfileUser/>
 			</div>
-		</>
+		</div>
 	);
 }
