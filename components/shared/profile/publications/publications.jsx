568ddc335d1c62b98ec684359a4bd0549b49c642
Dikiy Perec <hopabsq@mail.ru>
2024-12-20 17:53:39 +0600

    added content switcher in profile (made on tv)

--- components/shared/profile/publications/publications.jsx
+++ components/shared/profile/publications/publications.jsx
@@ -3,13 +3,36 @@ import { EffectCreative, FreeMode } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/effect-creative";
+import { useState, useEffect } from "react";
 
 export default function Publications() {
+	const [active, setActive] = useState(0);
+
+	useEffect(() => {
+		document.getElementById("content-switcher")?.scrollTo({
+			left: document.getElementById(`switcher-button-${active}`).offsetLeft - 20,
+			behavior: "smooth",
+			block: "start",
+		});
+	}, [active]);
+
 	return (
-		<>
+		<div className='flex flex-col gap-4'>
+			<div id="content-switcher" className='flex gap-6 text-white font-medium text-2xl px-5 overflow-x-auto'>
+				<button id="switcher-button-0" data-active={active === 0} className='duration-200 ease-out data-[active=true]:opacity-100 opacity-50'>
+					Publications
+				</button>
+				<button id="switcher-button-1" data-active={active === 1} className='duration-200 ease-out data-[active=true]:opacity-100 opacity-50'>
+					Comments
+				</button>
+				<button id="switcher-button-2" data-active={active === 2} className='duration-200 ease-out data-[active=true]:opacity-100 opacity-50'>
+					Reactions
+				</button>
+			</div>
 			<Swiper
 				resistance={true}
 				resistanceRatio={0}
+				onSlideChange={slide => setActive(slide.activeIndex)}
 				creativeEffect={{
 					prev: {
 						opacity: 0,
@@ -35,6 +58,6 @@ export default function Publications() {
 					<img src='https://swiperjs.com/demos/images/nature-3.jpg' />
 				</SwiperSlide>
 			</Swiper>
-		</>
+		</div>
 	);
 }
